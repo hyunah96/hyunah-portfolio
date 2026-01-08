55a1d9d7a3af7a83c984d0acacfe7bbb06a07884
hyunah96 <gusdk6_@naver.com>
2026-01-08 15:16:19 +0900

    update notes : 2026-01-08 15:16:19

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -86,6 +86,12 @@ export default (() => {
         <meta name="description" content={description} />
         <meta name="generator" content="Quartz" />
 
+        <script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token": "3161b3e645704d468b0e4e922e3b6f14"}'
+        />
+
         {css.map((resource) => CSSResourceToStyleElement(resource, true))}
         {js
           .filter((resource) => resource.loadTime === "beforeDOMReady")
